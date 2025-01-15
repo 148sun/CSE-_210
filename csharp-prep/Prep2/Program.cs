1890e1ad03f32ba22b6bcff0d48fac248e880a13
Amanda Barlow <bar20117@byui.edu>
2025-01-15 06:14:36 -0700

    prep 2 and creating another sandbox

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+Console.Write("What is your grade percentage? ");
+string yourGrade = Console.ReadLine();
+int gradePercentage = int.Parse(yourGrade);
+string Letter = "hi";
+if (gradePercentage >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    Letter = "A";
+}
+else if (gradePercentage >= 80)
+{
+    Letter = "B";
+}
+else if (gradePercentage >= 70)
+{
+    Letter = "C";
+}
+else if (gradePercentage >= 60)
+{
+        Letter = "D";
+
+
+}
+else
+{
+        Letter = "D";
+
+}
+if (gradePercentage >= 70)
+{
+    Console.WriteLine($"\nCongratulations, you passed the class with the grade {Letter}!! ");
+}
+else
+{
+    Console.WriteLine($"\nYou sadly did not pass the class. Your grade was {Letter}. I recommend trying again. ");
 }
